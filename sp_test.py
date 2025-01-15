c059c76704561890cf135675d931f42838f78d06
Sayan Naskar <sayan.naskar@clirnet.com>
2025-01-15 14:50:36 +0530

    test

--- sp_test.py
+++ sp_test.py
@@ -1,3 +1,12 @@
-a=1
-b=2
-print (a+b)
+import pyspark 
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("Read DataFrame Example") \
+    .getOrCreate()
+spark
+df = spark.read.csv("recording_request.csv", header=True, inferSchema=True)
+city_zone_df=df.groupBy("City","Zone").count()
+speciality_zone_df=df.groupBy("Speciality","Zone").count()
+joined_df = city_zone_df.join(speciality_zone_df, on="Zone", how="inner")
+joined_df.show()
