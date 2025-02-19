314e7b8c9525155d3bd22257e61eba68395055b4
sayankgp <naskarsayan07@gmail.com>
2025-02-20 00:40:43 +0530

    personal laptop

--- code_1.py
+++ code_1.py
@@ -1,4 +1,13 @@
-import pandas as pd
+from pyspark import SparkConf, SparkContext
+import collections
 
-data=pd.read_csv("recording_request.csv",low_memory=False)
-data
+conf=SparkConf().setMaster("local").setAppName("RatingHistogram")
+sc=SparkContext(conf=conf)
+
+lines=sc.textFile("ml-100k/u.data")
+ratings=lines.map(lambda x:x.split()[2])
+result=ratings.countByValue()
+
+sortedResults=collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.iteritems():
+    print ("%s %i") % (key, value)
